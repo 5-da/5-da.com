4f8980173b82fd9f6693327c7cc5f16b9347ccc8
Shuichiro Goda <shuichiro@shuichiro.local>
2017-10-22 01:13:33 +0900

    スマホのことだけ考えた

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php do_action( 'main_bottom' ); ?>
+</section><!-- .main -->
+</div>
+</div><!-- .overflow-container -->
+
+<?php do_action( 'body_bottom' ); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
